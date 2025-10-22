b578c76d521fc3a3b6b5150a032c4aac6b6b6ea0
Just1a2Noob <kamikira.hd@gmail.com>
2025-10-22 21:40:41 +0700

    Initial commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const Connection = "amqp://guest:guest@localhost:5672"
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	conn, err := amqp.Dial(Connection)
+	if err != nil {
+		log.Fatalf("Cannot connect to server %s", err)
+	}
+
+	// To ensure the connection is closed when the program exits
+	defer conn.Close()
+
+	fmt.Printf("Connection to server was successful")
+
+	// Using signal channels, we can listen for signals i.e. Ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	// Were waiting for a value to receive then active the code below it
+	<-signalChan
+
+	fmt.Printf("\nClossing connection...")
 }
