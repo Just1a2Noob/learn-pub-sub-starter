7c12d8f02c2dcda91978503d7e98a77ded23b2c8
Just1a2Noob <kamikira.hd@gmail.com>
2025-10-23 13:38:22 +0700

    Updated main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -15,12 +17,31 @@ func main() {
 	conn, err := amqp.Dial(Connection)
 	if err != nil {
 		log.Fatalf("Cannot connect to server %s", err)
+		return
 	}
 
 	// To ensure the connection is closed when the program exits
 	defer conn.Close()
 
+	connChan, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Error creating connection channel: %s", err)
+	}
+
 	fmt.Printf("Connection to server was successful")
+
+	// Below publishes an exchange message
+	err = pubsub.PublishJSON(
+		connChan,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{IsPaused: true},
+	)
+
+	if err != nil {
+		log.Fatalf("Error publishing message: %s", err)
+		return
+	}
 
 	// Using signal channels, we can listen for signals i.e. Ctrl+c
 	signalChan := make(chan os.Signal, 1)
