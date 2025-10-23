07b56739cf5b80a25eb68e841f6462da21661036
Just1a2Noob <kamikira.hd@gmail.com>
2025-10-23 13:38:16 +0700

    Added PublishJson function to exchange messangges

---
+++ internal/pubsub/publish_json.go
@@ -0,0 +1,27 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	data, err := json.Marshal(val)
+	if err != nil {
+		log.Fatalf("Error marshalling value to json: %s", err)
+	}
+
+	publish_json := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        data,
+	}
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, publish_json)
+	if err != nil {
+		log.Fatalf("Error publishing with context: %s", err)
+	}
+
+	return nil
+}
