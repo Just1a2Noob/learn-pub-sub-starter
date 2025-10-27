a63e466144fea9f50c9e9364a096056abf4c9d9f
Just1a2Noob <kamikira.hd@gmail.com>
2025-10-27 09:47:05 +0700

    Added a new return for Ack and Nack

--- cmd/client/handlerMove.go
+++ cmd/client/handlerMove.go
@@ -4,11 +4,27 @@ import (
 	"fmt"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 )
 
-func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
-	return func(move gamelogic.ArmyMove) {
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.Acktype {
+	return func(move gamelogic.ArmyMove) pubsub.Acktype {
 		defer fmt.Print(">")
-		gs.HandleMove(move)
+		outcome := gs.HandleMove(move)
+
+		switch outcome {
+		case gamelogic.MoveOutcomeSamePlayer:
+			fmt.Print("NackDiscard\n")
+			return pubsub.NackDiscard
+		case gamelogic.MoveOutComeSafe:
+			fmt.Print("Acknowledge\n")
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeMakeWar:
+			fmt.Print("Acknowledge\n")
+			return pubsub.Ack
+		default:
+			fmt.Print("NackDiscard\n")
+			return pubsub.NackDiscard
+		}
 	}
 }
--- cmd/client/handlerPause.go
+++ cmd/client/handlerPause.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
-func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
-	return func(ps routing.PlayingState) {
+func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.Acktype {
+	return func(ps routing.PlayingState) pubsub.Acktype {
 		defer fmt.Print("> ")
 		gs.HandlePause(routing.PlayingState{
 			IsPaused: ps.IsPaused,
 		})
+		fmt.Print("Acknowledge")
+		return pubsub.Ack
 	}
 }
